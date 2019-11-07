c3a66da685fd7ff16bfe4519acbea8e64cd598d8
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-07 23:35:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
-  newObj[key] = value;
+  newObj[key];
   delete newObj[key];
  return newObj; 
 }
